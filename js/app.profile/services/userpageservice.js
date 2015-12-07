6ce8f6a6f63a5e9ca8e5f2742ede41c9a6e3dd94
Jonny Warren <jonnywarren06@gmail.com>
2015-12-07 17:11:20 -0500

    users can now delete posts

--- js/app.profile/services/userpageservice.js
+++ js/app.profile/services/userpageservice.js
@@ -1,12 +1,16 @@
 let UserPageService = function(SERVER, FILESERVER, $cookies, $http, UserService) {
 
   this.getAllPosts = getAllPosts;
+  this.deletePost = deletePost;
 
   function getAllPosts (id) {
-    console.log(id);
     UserService.checkAuth();
     UserService.checkFileAuth();
     return $http.get(FILESERVER.URL + 'users/' + id + '/posts', FILESERVER.CONFIG);
+  }
+
+  function deletePost (id) {
+    return $http.delete(FILESERVER.URL + 'posts/' + id, FILESERVER.CONFIG);
   }
 
 };
--- js/app.projects/controllers/singlepostcontroller.js
+++ js/app.projects/controllers/singlepostcontroller.js
@@ -1,14 +1,29 @@
-let SinglePostController = function($stateParams, ProjectService, UserPageService) {
+let SinglePostController = function($state, $stateParams, UserService, ProfileService, ProjectService, UserPageService) {
   
   let vm = this;
+
+  vm.deletePost = deletePost;
 
   ProjectService.getPost($stateParams.id).then( (res) => {
     console.log(res.data.post[0]);
     vm.post = res.data.post[0];
+    vm.userId = res.data.post[0].user_id;
+    // console.log(vm.userId);
+    return vm.userId;
   });
+
+  let id = $stateParams.id;
+
+  function deletePost (userId) {
+    console.log('fucking delete it');
+    UserService.checkFileAuth();
+    UserPageService.deletePost(id).then( (res) => {
+      $state.go('root2.userhome', {id: userId});
+    });
+  }
 
 };
 
-SinglePostController.$inject = ['$stateParams', 'ProjectService', 'UserPageService'];
+SinglePostController.$inject = ['$state', '$stateParams', 'UserService', 'ProfileService', 'ProjectService', 'UserPageService'];
 
 export default SinglePostController;
